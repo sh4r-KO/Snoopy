f3a887fe2e20043dddb0e4df663d6028781d85bf
sh4r-KO <aguirrecabrol5@gmail.com>
2022-11-09 21:46:58 +0100

    Update Main.java

--- src/main/java/com/example/snoopy/Main.java
+++ src/main/java/com/example/snoopy/Main.java
@@ -9,14 +9,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    //******************************************************************************************************************
+    //---------------------------------A NE PAS SUPPRIMIER MEME SI YA EDS DOUBLONS OK ????------------------------------
+    //******************************************************************************************************************
+    /*
+    TODO: reduce visibilty of all elements in contorllers like public button -> private
+    TODO: make a super controller and add redundances method such as ReturnCliecked (if difference faced, re-write or add parameters)
+    TODO: make all element in fxml responsive/relative to the size
+    TODO: super cntroler avec la methode dec hangement de fichier fxml
 
-    //TODO: reduce visibilty of all elements in contorllers like public button -> private
-    //TODO: make a super controller and add redundances method such as ReturnCliecked (if difference faced, re-write or add parameters)
-    //TODO: make all element in fxml responsive/relative to the size
-    //TODO: super cntroler avec la methode dec hangement de fichier fxml
-    //commebtaire
-    //comentaires sa mere
-    //commentaire sa race
+    */
+    //******************************************************************************************************************
+    //******************************************************************************************************************
+    //******************************************************************************************************************
+
     public static final int Stage_width = 1440;//75% of 1920
     public static final int Stage_height = 810;//75% of 1080
 
